24046b10dc9f0e5cb2a42e2418007a23025cfdf1
Jared Farkas and Riley Benson <riley@>
2019-01-15 10:51:13 -0800

    add toggle all words

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,79 @@
 $(document).ready(function() {
-  $(".well").click(function() {
-    $("p").toggle();
-    $("h3").toggle();
+  $(".well#JS").click(function() {
+    $("#JS p").toggleClass("hidden");
+    $("#JS h3").toggleClass("hidden");
   });
+  $(".well#OP").click(function() {
+    $("#OP p").toggleClass("hidden");
+    $("#OP h3").toggleClass("hidden");
+  });
+  $(".well#V").click(function() {
+    $("#V p").toggleClass("hidden");
+    $("#V h3").toggleClass("hidden");
+  });
+  $(".well#VNC").click(function() {
+    $("#VNC p").toggleClass("hidden");
+    $("#VNC h3").toggleClass("hidden");
+  });
+  $(".well#FU").click(function() {
+    $("#FU p").toggleClass("hidden");
+    $("#FU h3").toggleClass("hidden");
+  });
+  $(".well#MT").click(function() {
+    $("#MT p").toggleClass("hidden");
+    $("#MT h3").toggleClass("hidden");
+  });
+  $(".well#ARG").click(function() {
+    $("#ARG p").toggleClass("hidden");
+    $("#ARG h3").toggleClass("hidden");
+  });
+  $(".well#PM").click(function() {
+    $("#PM p").toggleClass("hidden");
+    $("#PM h3").toggleClass("hidden");
+  });
+  $(".well#RT").click(function() {
+    $("#RT p").toggleClass("hidden");
+    $("#RT h3").toggleClass("hidden");
+  });
+  $(".well#CM").click(function() {
+    $("#CM p").toggleClass("hidden");
+    $("#CM h3").toggleClass("hidden");
+  });
+  $(".well#STR").click(function() {
+    $("#STR p").toggleClass("hidden");
+    $("#STR h3").toggleClass("hidden");
+  });
+  $(".well#BL").click(function() {
+    $("#BL p").toggleClass("hidden");
+    $("#BL h3").toggleClass("hidden");
+  });
+  $(".well#UN").click(function() {
+    $("#UN p").toggleClass("hidden");
+    $("#UN h3").toggleClass("hidden");
+  });
+  $(".well#NN").click(function() {
+    $("#NN p").toggleClass("hidden");
+    $("#NN h3").toggleClass("hidden");
+  });
+  $(".well#EC").click(function() {
+    $("#EC p").toggleClass("hidden");
+    $("#EC h3").toggleClass("hidden");
+  });
+  $(".well#AL").click(function() {
+    $("#AL p").toggleClass("hidden");
+    $("#AL h3").toggleClass("hidden");
+  });
+  $(".well#CT").click(function() {
+    $("#CT p").toggleClass("hidden");
+    $("#CT h3").toggleClass("hidden");
+  });
+  $(".well#JQ").click(function() {
+    $("#JQ p").toggleClass("hidden");
+    $("#JQ h3").toggleClass("hidden");
+  });
+  $(".well#ATB").click(function() {
+    $("#ATB p").toggleClass("hidden");
+    $("#ATB h3").toggleClass("hidden");
+  });
+
 });
